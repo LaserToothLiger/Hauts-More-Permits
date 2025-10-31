9b253448963928aaa5432be0813bff7eb1cd1bc8
LaserToothLiger <lasertoothliger@gmail.com>
2025-10-31 07:55:25 -0700

    v1.2.01
    Buffed and coolified all non-round Don't Forget monsters. Removed a couple cosmetic elements in DLC-dependent content that were dependent on other DLCs. Grandsplice chimera inherits alternate graphics from chimera.

--- 1.6/Mods/Anomaly/Source/HautsPermits_Occult/Class1.cs
+++ 1.6/Mods/Anomaly/Source/HautsPermits_Occult/Class1.cs
@@ -26,6 +26,7 @@ using DelaunatorSharp;
 using HarmonyLib;
 using VEF.Abilities;
 using static System.Net.Mime.MediaTypeNames;
+using System.Net.NetworkInformation;
 
 namespace HautsPermits_Occult
 {
@@ -43,6 +44,8 @@ namespace HautsPermits_Occult
                           prefix: new HarmonyMethod(patchType, nameof(HVMPCompGrayStatueTeleporter_TriggerPrefix)));
             harmony.Patch(AccessTools.Method(typeof(UnnaturalCorpseTracker), nameof(UnnaturalCorpseTracker.CorpseTick)),
                           postfix: new HarmonyMethod(patchType, nameof(HVMPCorpseTickPostfix)));
+            HVMPDefOf_A.HVMP_GrandspliceChimera.alternateGraphics = PawnKindDefOf.Chimera.alternateGraphics;
+            HVMPDefOf_A.HVMP_GrandspliceChimera.alternateGraphicChance = PawnKindDefOf.Chimera.alternateGraphicChance;
         }
         public static void HVMPCompFloorEtching_PostSpawnSetupPostfix(CompFloorEtching __instance)
         {
@@ -118,6 +121,7 @@ namespace HautsPermits_Occult
 
         public static PawnKindDef HVMP_PrimedNociosphere;
         public static PawnKindDef HVMP_GhoulSuperEvil;
+        public static PawnKindDef HVMP_GrandspliceChimera;
 
         public static ThingDef HVMP_LivingCloudkill;
 
@@ -3247,6 +3251,127 @@ namespace HautsPermits_Occult
         private float direction;
         private static ModuleBase directionNoise;
     }
+    public class HediffCompProperties_AcidMortarOnHit : HediffCompProperties
+    {
+        public HediffCompProperties_AcidMortarOnHit()
+        {
+            this.compClass = typeof(HediffComp_AcidMortarOnHit);
+        }
+        public float chance;
+        public int cooldown;
+        public ThingDef projectile;
+        public float range;
+        public float forcedMissRadius = 1.2f;
+    }
+    public class HediffComp_AcidMortarOnHit : HediffComp
+    {
+        public HediffCompProperties_AcidMortarOnHit Props
+        {
+            get
+            {
+                return (HediffCompProperties_AcidMortarOnHit)this.props;
+            }
+        }
+        public override void CompPostTickInterval(ref float severityAdjustment, int delta)
+        {
+            base.CompPostTickInterval(ref severityAdjustment, delta);
+            if (this.cooldown > 0)
+            {
+                this.cooldown -= delta;
+            }
+        }
+        public override void Notify_PawnPostApplyDamage(DamageInfo dinfo, float totalDamageDealt)
+        {
+            base.Notify_PawnPostApplyDamage(dinfo, totalDamageDealt);
+            if (this.cooldown <= 0 && this.Pawn.Spawned && Rand.Chance(this.Props.chance) && dinfo.Instigator != null)
+            {
+                Thing t = dinfo.Instigator;
+                if (t.SpawnedOrAnyParentSpawned && t.MapHeld == this.Pawn.Map)
+                {
+                    ThingDef proj = this.Props.projectile;
+                    Projectile projectile2 = (Projectile)GenSpawn.Spawn(proj, this.Pawn.Position, this.Pawn.Map, WipeMode.Vanish);
+                    float num2 = VerbUtility.CalculateAdjustedForcedMiss(this.Props.forcedMissRadius, t.PositionHeld - this.Pawn.Position);
+                    if (num2 > 0.5f)
+                    {
+                        IntVec3 forcedMissTarget = this.GetForcedMissTarget(num2, t.PositionHeld);
+                        if (forcedMissTarget != t.PositionHeld)
+                        {
+                            ProjectileHitFlags projectileHitFlags = ProjectileHitFlags.NonTargetWorld;
+                            if (Rand.Chance(0.5f))
+                            {
+                                projectileHitFlags = ProjectileHitFlags.All;
+                            }
+                            projectile2.Launch(this.Pawn, this.Pawn.DrawPos, forcedMissTarget, t, projectileHitFlags, false, null, null);
+                            return;
+                        }
+                    }
+                    ProjectileHitFlags projectileHitFlags2 = ProjectileHitFlags.IntendedTarget;
+                    projectile2.Launch(this.Pawn, this.Pawn.DrawPos, t, t, projectileHitFlags2, false, null, null);
+                }
+            }
+        }
+        public IntVec3 GetForcedMissTarget(float forcedMissRadius, IntVec3 targetPosition)
+        {
+            int num = GenRadial.NumCellsInRadius(forcedMissRadius);
+            int num2 = Rand.Range(0, num);
+            return targetPosition + GenRadial.RadialPattern[num2];
+        }
+        public override void CompExposeData()
+        {
+            base.CompExposeData();
+            Scribe_Values.Look<int>(ref this.cooldown, "cooldown", 0, false);
+        }
+        public int cooldown = 0;
+    }
+    public class HediffCompProperties_FleshbeastsOnDeath : HediffCompProperties
+    {
+        public HediffCompProperties_FleshbeastsOnDeath()
+        {
+            this.compClass = typeof(HediffComp_FleshbeastsOnDeath);
+        }
+        public IntRange fleshbeastCount;
+        public List<PawnKindDef> fleshbeastKinds;
+    }
+    public class HediffComp_FleshbeastsOnDeath : HediffComp
+    {
+        public HediffCompProperties_FleshbeastsOnDeath Props
+        {
+            get
+            {
+                return (HediffCompProperties_FleshbeastsOnDeath)this.props;
+            }
+        }
+        public override void Notify_PawnDied(DamageInfo? dinfo, Hediff culprit = null)
+        {
+            base.Notify_PawnDied(dinfo, culprit);
+            if (this.Pawn.SpawnedOrAnyParentSpawned)
+            {
+                Faction f = this.Pawn.Faction ?? Faction.OfEntities;
+                Lord lord = this.Pawn.lord ?? LordMaker.MakeNewLord(f, new LordJob_AssaultColony(f, false, false, false, false, false, false, false), this.Pawn.MapHeld, null);
+                int count = this.Props.fleshbeastCount.RandomInRange;
+                for (int i = count; i > 0; i--)
+                {
+                    Pawn pawn2 = PawnGenerator.GeneratePawn(new PawnGenerationRequest(this.Props.fleshbeastKinds.RandomElement(), f, PawnGenerationContext.NonPlayer, null, true, false, false, true, false, 1f, false, true, false, true, true, false, false, false, false, 0f, 0f, null, 1f, null, null, null, null, null, 0f, null, null, null, null, null, null, false, false, false, false, null, null, null, null, null, 0f, DevelopmentalStage.Adult, null, null, null, false, false, false, -1, 0, false));
+                    this.SpawnPawn(pawn2, this.Pawn.PositionHeld, this.Pawn.MapHeld, lord);
+                }
+            }
+            FleshbeastUtility.MeatSplatter(Rand.RangeInclusive(1,3), this.Pawn.PositionHeld, this.Pawn.MapHeld, FleshbeastUtility.ExplosionSizeFor(this.Pawn));
+            FilthMaker.TryMakeFilth(this.Pawn.PositionHeld, this.Pawn.MapHeld, ThingDefOf.Filth_TwistedFlesh, 1, FilthSourceFlags.None, true);
+            if (this.Pawn.Corpse != null)
+            {
+                this.Pawn.Corpse.Destroy();
+            }
+        }
+        private void SpawnPawn(Pawn child, IntVec3 position, Map map, Lord lord)
+        {
+            GenSpawn.Spawn(child, position, map, WipeMode.VanishOrMoveAside);
+            if (lord != null)
+            {
+                lord.AddPawn(child);
+            }
+            FleshbeastUtility.SpawnPawnAsFlyer(child, map, position, 5, true);
+        }
+    }
     public class HediffCompProperties_Umbranarch : HediffCompProperties
     {
         public HediffCompProperties_Umbranarch()
@@ -3256,7 +3381,8 @@ namespace HautsPermits_Occult
         public IntRange noctolCount;
         public int noctolCooldown;
         public PawnKindDef noctolDef;
-        public string darknessLetterText;
+        public EffecterDef effecter;
+        public QuestScriptDef darkness;
     }
     public class HediffComp_Umbranarch : HediffComp
     {
@@ -3267,6 +3393,15 @@ namespace HautsPermits_Occult
                 return (HediffCompProperties_Umbranarch)this.props;
             }
         }
+        public override void CompPostTick(ref float severityAdjustment)
+        {
+            base.CompPostTick(ref severityAdjustment);
+            if (this.effecter == null)
+            {
+                this.effecter = this.Props.effecter.Spawn();
+            }
+            this.effecter.EffectTick(this.Pawn, this.Pawn);
+        }
         public override void CompPostTickInterval(ref float severityAdjustment, int delta)
         {
             base.CompPostTickInterval(ref severityAdjustment, delta);
@@ -3274,22 +3409,21 @@ namespace HautsPermits_Occult
             {
                 this.spawnCooldown -= delta;
             }
-            if (this.Pawn.Spawned && this.Pawn.IsHashIntervalTick(2500))
+            if (this.Pawn.Spawned && this.Pawn.IsHashIntervalTick(360))
             {
                 if (this.Pawn.Map.GetComponent<HVMP_HypercubeMapComponent>() == null)
                 {
                     GameCondition gcud = this.Pawn.Map.gameConditionManager.GetActiveCondition(GameConditionDefOf.UnnaturalDarkness);
                     if (gcud == null)
                     {
-                        GameCondition gameCondition = GameConditionMaker.MakeCondition(GameConditionDefOf.UnnaturalDarkness, 99999);
-                        gameCondition.Duration = 4000;
-                        if (gameCondition.CanApplyOnMap(this.Pawn.Map))
+                        Slate slate = new Slate();
+                        slate.Set<TaggedString>("discoveryMethod", "QuestDiscoveredFromDebug".Translate(), false);
+                        slate.Set<float>("points", StorytellerUtility.DefaultThreatPointsNow(this.Pawn.Map),false);
+                        if (this.Props.darkness.CanRun(slate,this.Pawn.Map))
                         {
-                            this.Pawn.Map.gameConditionManager.RegisterCondition(gameCondition);
-                            Find.LetterStack.ReceiveLetter(gameCondition.LabelCap, this.Props.darknessLetterText, LetterDefOf.ThreatSmall, this.Pawn, null, null, null, null, 0, true);
+                            QuestUtility.GenerateQuestAndMakeAvailable(this.Props.darkness, slate);
+                            EffecterDefOf.MonolithLevelChanged.Spawn().Trigger(new TargetInfo(this.Pawn.Position, this.Pawn.Map, false), new TargetInfo(this.Pawn.Position, this.Pawn.Map, false), -1);
                         }
-                    } else {
-                        gcud.TicksLeft = Math.Max(gcud.TicksLeft,4000);
                     }
                 }
             }
@@ -3325,6 +3459,7 @@ namespace HautsPermits_Occult
             Scribe_Values.Look<int>(ref this.spawnCooldown, "spawnCooldown", 0, false);
         }
         public int spawnCooldown;
+        private Effecter effecter;
     }
     //romero
     public class QuestNode_Romero_BS : QuestNode
