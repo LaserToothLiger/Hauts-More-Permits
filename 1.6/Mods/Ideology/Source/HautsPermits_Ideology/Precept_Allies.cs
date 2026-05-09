5aad925f1139f8bc7896b8311e203d7147aece28
LaserToothLiger <lasertoothliger@gmail.com>
2026-05-08 22:50:51 -0700

    v1.8.05
    Redone art for gatepuncher, new art for Remnant quest items. Cosmopolitan meme has a new exclusive precept which imposes soft pressure on gameplay, justifying its two-pip impact rating.

---
+++ 1.6/Mods/Ideology/Source/HautsPermits_Ideology/Precept_Allies.cs
@@ -0,0 +1,42 @@
+﻿using RimWorld;
+using Verse;
+
+namespace HautsPermits_Ideology
+{
+    public class ThoughtWorker_Precept_NetAllyToFoeCount : ThoughtWorker_Precept
+    {
+        protected override ThoughtState ShouldHaveThought(Pawn p)
+        {
+            int num = ThoughtWorker_Precept_NetAllyToFoeCount.NetAllyCount(p);
+            if (num > 0)
+            {
+                return ThoughtState.ActiveAtStage(0);
+            } else if (num < 0) {
+                return ThoughtState.ActiveAtStage(1);
+            } else {
+                return ThoughtState.Inactive;
+            }
+        }
+        public static int NetAllyCount(Pawn p)
+        {
+            int num = 0;
+            if (p.Faction != null)
+            {
+                Faction pawnFac = p.Faction;
+                foreach (Faction f in Find.FactionManager.AllFactionsVisible)
+                {
+                    if (f != pawnFac)
+                    {
+                        if (pawnFac.RelationKindWith(f) == FactionRelationKind.Ally)
+                        {
+                            num++;
+                        } else if (pawnFac.RelationKindWith(f) == FactionRelationKind.Hostile) {
+                            num--;
+                        }
+                    }
+                }
+            }
+            return num;
+        }
+    }
+}
